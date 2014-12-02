d88c6dbb5c4bf1a08e4fab7541a90709c0c34a8f
Ed Bueler <elbueler@alaska.edu>
2014-12-01 20:19:34 -0700

    start on the pictures I really want

--- agutalk/figs1d.py
+++ agutalk/figs1d.py
@@ -3,18 +3,20 @@
 from pylab import *
 from sys import exit
 
-th = linspace(0.0,2.0*pi,1001)
+x = linspace(0.0,10.0,1001)
+b = 0.07*(x-3.0)**2 + 0.2*sin(2.0*x)
 
-pert = 0.6*exp(-(th-4.3)**2/0.3)
-xOm = cos(th) + pert
-yOm = sin(th) + pert
+h0 = 3.0
+L = 3.0
+h = maximum(0.0, h0*(-0.2 + sqrt(maximum(0.0,1.0 - (x-5)**2/L**2))) )
+s = b + h
 
-figure(figsize=(6,6))
-plot(xOm, yOm, '-k', lw=2.0)
-#hold(True)
+figure(figsize=(6,2))
+plot(x, b, '-k', lw=2.0)
+hold(True)
+plot(x, s, 'k', lw=2.0)
 
-text(xOm[150]+0.12,yOm[150]+0.12,r'$\Omega$',fontsize=20.0)
-
+#text(xOm[150]+0.12,yOm[150]+0.12,r'$\Omega$',fontsize=20.0)
 #arrow(xOm[650]+0.15,yOm[650]-0.25,-0.1,0.2)
 
 axis('off')
