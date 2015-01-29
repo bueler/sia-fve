8cfc8b18938c6be872e106b1cbe37ad3799f616a
Ed Bueler <elbueler@alaska.edu>
2015-01-28 20:25:50 -0700

    generate figures better

--- talks/refinemass.sh
+++ talks/refinemass.sh
@@ -5,7 +5,7 @@ NN=1
 for MM in 0 1 2 ; do
   STEPS=$(bc -l <<< "100*10^$MM")
   DT=$(bc -l <<< "scale = 6; 10.0/$STEPS")
-  cmd="mpiexec -n $NN ./layer -da_refine 5 -lay_dt ${DT} -lay_steps ${STEPS} -lay_adscheme 1 -lay_jac -lay_noshow -lay_timedependentsource -lay_massfile mass${MM}.txt -snes_max_it 1000"
+  cmd="mpiexec -n $NN ../petsc/layer -da_refine 5 -lay_dt ${DT} -lay_steps ${STEPS} -lay_adscheme 1 -lay_jac -lay_noshow -lay_timedependentsource -lay_massfile mass${MM}.txt -snes_max_it 1000"
   echo $cmd
   $cmd
 done
