e436962af2106aa12d1466d1cdd81382e0eee3b1
Ed Bueler <elbueler@alaska.edu>
2015-03-15 15:35:49 -0600

    Minimal download and 150m-->900m coarsening script.

---
+++ petsc/mahaffy/grn/getmcb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -nc ftp://sidads.colorado.edu/DATASETS/IDBMG4_BedMachineGr/MCdataset-2014-11-19.nc
+
+ncks -v x,y,thickness,bed MCdataset-2014-11-19.nc -d x,,,6 -d y,,,6 mcb900m.nc
+
