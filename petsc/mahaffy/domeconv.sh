9887025062b6c5f6edf65ac8b57df280ff9de7f7
Ed Bueler <elbueler@alaska.edu>
2015-03-17 14:07:41 -0600

    Add regression test of classical Mahaffy (-mah_true).

--- petsc/mahaffy/domeconv.sh
+++ petsc/mahaffy/domeconv.sh
@@ -3,12 +3,14 @@
 # refinement path for dome test
 # for -da_refine levels 0 ... 4 in file foo.verif, on 6 processes, do
 #   $ ./domeconv.sh foo.verif 4 6
+# for same plus -mah_true:
+#   $ ./domeconv.sh foo.verif 4 6 -mah_true
 
 # creates and destroys files lev?history.txt
 
 for lev in `seq 0 $2`; do
    rm -f lev${lev}history.txt
-   mpiexec -n $3 ./mahaffy -da_refine $lev -snes_max_it 200 -pc_type asm -sub_pc_type lu -mah_history lev${lev}
+   mpiexec -n $3 ./mahaffy $4 -da_refine $lev -snes_max_it 200 -pc_type asm -sub_pc_type lu -mah_history lev${lev}
 done
 
 rm -f $1
