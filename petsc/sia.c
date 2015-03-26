49a73c4a088ef023cffc0c6563fabbf09bec311b
Ed Bueler <elbueler@alaska.edu>
2015-03-25 22:07:24 -0600

    Minor fix and reorg.

--- petsc/sia.c
+++ petsc/sia.c
@@ -3,6 +3,8 @@
 #include "q1op.h"
 #include "sia.h"
 
+// ******** FUNCTIONS ********
+
 /*
 Compute delta, a factor of both the diffusivity D and the pseudo-velocity W,
 from values of thickness and surface gradient:
@@ -26,6 +28,26 @@ PetscReal Dcont(PetscReal delta, PetscReal H, const AppCtx *user) {
   return (1.0-user->eps) * delta * PetscPowReal(H,n+2.0) + user->eps * user->D0;
 }
 
+
+/* See sia.h for doc. */
+PetscReal getflux(Grad gH, Grad gb, PetscReal H, PetscReal Hup,
+                  PetscBool xdir, AppCtx *user) {
+  const PetscReal n     = ncont(user),
+                  delta = getdelta(gH,gb,user->Gamma,n),
+                  D     = Dcont(delta,H,user);
+  user->maxD = PetscMax(user->maxD, D);
+  if (xdir == PETSC_TRUE) {
+      const PetscReal  Wx = - delta * gb.x;
+      return - D * gH.x + Wx * PetscPowReal(Hup,n+2.0);
+  } else {
+      const PetscReal  Wy = - delta * gb.y;
+      return - D * gH.y + Wy * PetscPowReal(Hup,n+2.0);
+  }
+}
+
+
+// ******** DERIVATIVES ********
+
 /*
 Derivative of delta with respect to components of gH.  Returns Grad ddH with two
 components:
@@ -43,7 +65,7 @@ Grad DdeltaDgH(Grad gH, Grad gb, PetscReal Gamma, PetscReal n) {
 }
 
 /*
-Derivative of delta with respect to corner thickness H_l, at point (x,y) on
+Derivative of delta with respect to nodal value H_l, at point (x,y) on
 element u,v:
    d delta / dl = (d delta / d gH.x) * (d gH.x / dl) + (d delta / d gH.y) * (d gH.y / dl)
 */
@@ -59,27 +81,26 @@ PetscReal DdeltaDl(PetscInt l,
 }
 
 /*
-Derivative of diffusivity D with respect to corner thickness H_l, at point (x,y) on
-element u,v.  Since  D = delta H^{n+2}:
-   d D / dl = (d delta / dl) H^{n+2} + delta (n+2) H^{n+1} (d H / dl)
+Derivative of continuation diffusivity D with respect to nodal value H_l, at
+point (x,y) on element u,v.  Since  Dcont = (1-eps) delta H^{n+2} + eps D0:
+   d Dcont / dl = (1-eps) [ (d delta / dl) H^{n+2} + delta (n+2) H^{n+1} (d H / dl) ]
+                = (1-eps) H^{n+1} [ (d delta / dl) H + delta (n+2) (d H / dl) ]
 */
-PetscReal DDDl(PetscInt l,
-               PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-               PetscReal **H, PetscReal **b, const AppCtx *user) {
+PetscReal DDcontDl(PetscInt l,
+                   PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
+                   PetscReal delta,
+                   PetscReal **H, PetscReal **b, const AppCtx *user) {
     const PetscReal n = ncont(user);
-    // delta could be an argument to this method, in which case this not needed:
-    const Grad gH = gradfatpt(u,v,locx,locy,H,user),
-               gb = gradfatpt(u,v,locx,locy,b,user);
-    const PetscReal delta = getdelta(gH,gb,user->Gamma,n);
     // get d/dl values:
-    const PetscReal Hatpt    =    fieldatpt(u,v,locx,locy,H,user),
+    const PetscReal Hatpt    = fieldatpt(u,v,locx,locy,H,user),
+                    Hpow     = PetscPowReal(Hatpt,n+1.0),
                     dHdl     = dfieldatpt(l,u,v,locx,locy,user),
                     ddeltadl =   DdeltaDl(l,u,v,locx,locy,H,b,user);
-    return ddeltadl * PetscPowReal(Hatpt,n+2.0) + delta * (n+2.0) * PetscPowReal(Hatpt,n+1.0) * dHdl;
+    return (1.0-user->eps) * Hpow * ( ddeltadl * Hatpt + delta * (n+2.0) * dHdl );
 }
 
 /*
-Derivative of pseudo-velocity W with respect to corner thickness H_l, at point
+Derivative of pseudo-velocity W with respect to nodal value H_l, at point
 (x,y) on element u,v.  Since  W = - delta * grad b:
    d W.x / dl = - (d delta / dl) * gb.x
    d W.y / dl = - (d delta / dl) * gb.y
@@ -98,20 +119,9 @@ Grad DWDl(PetscInt l,
 
 //FIXME since  q.x = - D gH.x + W.x Hup^{n+2}  we have:
 //   d q.x / dl = - (d D / dl) gH.x - D (d gH.x / dl) + (d W.x / dl) Hup^{n+2} + W.x (n+2) Hup^{n+1} (d Hup / dl)
-
-/* See sia.h for doc. */
-PetscReal getflux(Grad gH, Grad gb, PetscReal H, PetscReal Hup,
-                  PetscBool xdir, AppCtx *user) {
-  const PetscReal n     = ncont(user),
-                  delta = getdelta(gH,gb,user->Gamma,n),
-                  D     = Dcont(delta,H,user);
-  user->maxD = PetscMax(user->maxD, D);
-  if (xdir == PETSC_TRUE) {
-      const PetscReal  Wx = - delta * gb.x;
-      return - D * gH.x + Wx * PetscPowReal(Hup,n+2.0);
-  } else {
-      const PetscReal  Wy = - delta * gb.y;
-      return - D * gH.y + Wy * PetscPowReal(Hup,n+2.0);
-  }
-}
+/* will need code:
+    const Grad gH = gradfatpt(u,v,locx,locy,H,user),
+               gb = gradfatpt(u,v,locx,locy,b,user);
+    const PetscReal delta = getdelta(gH,gb,user->Gamma,n);
+*/
 
