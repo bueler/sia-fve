818bce747eac341a6145d3471f5a580a03e220fb
Ed Bueler <elbueler@alaska.edu>
2015-03-25 23:12:28 -0600

    Now getflux() and DfluxDl() have comparable arguments, and Q1 ops do not appear in sia.c.

--- petsc/sia.c
+++ petsc/sia.c
@@ -1,6 +1,5 @@
 /* (C) 2015 Ed Bueler */
 
-#include "q1op.h"
 #include "sia.h"
 
 // ******** FUNCTIONS ********
@@ -35,7 +34,6 @@ PetscReal Dcont(PetscReal delta, PetscReal H, const AppCtx *user) {
   return (1.0-user->eps) * delta * PetscPowReal(H,n+2.0) + user->eps * user->D0;
 }
 
-
 /* See sia.h for doc. */
 PetscReal getflux(Grad gH, Grad gb, PetscReal H, PetscReal Hup,
                   PetscBool xdir, AppCtx *user) {
@@ -55,35 +53,19 @@ PetscReal getflux(Grad gH, Grad gb, PetscReal H, PetscReal Hup,
 // ******** DERIVATIVES ********
 
 /*
-Derivative of delta with respect to components of gH.  Returns Grad ddH with two
-components:
-   ddH.x = d delta / d gH.x
-   ddH.y = d delta / d gH.y
-*/
-Grad DdeltaDgH(Grad gH, Grad gb, PetscReal Gamma, PetscReal n) {
-    const PetscReal sx  = gH.x + gb.x,
-                    sy  = gH.y + gb.y,
-                    tmp = Gamma * (n-1) * PetscPowReal(sx*sx + sy*sy,(n-3.0)/2);
-    Grad ddH;
-    ddH.x = tmp * sx;
-    ddH.y = tmp * sy;
-    return ddH;
-}
-
-/*
 Derivative of delta with respect to nodal value H_l, at point (x,y) on
 element u,v:
-   d delta / dl = (d delta / d gH.x) * (d gH.x / dl) + (d delta / d gH.y) * (d gH.y / dl)
+   d delta / dl = (d delta / d gH.x) * (d gH.x / dl) + (d delta / d gH.y) * (d gH.y / dl),
+but
+   d delta / d gH.x = Gamma * (n-1) * |gH + gb|^{n-3.0} * (gH.x + gb.x)
+   d delta / d gH.y = Gamma * (n-1) * |gH + gb|^{n-3.0} * (gH.y + gb.y)
 */
-PetscReal DdeltaDl(PetscInt l,
-                   PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                   PetscReal **H, PetscReal **b, const AppCtx *user) {
-    const PetscReal n = ncont(user);
-    const Grad gH    =    gradfatpt(u,v,locx,locy,H,user),
-               gb    =    gradfatpt(u,v,locx,locy,b,user),
-               dgHdl = dgradfatpt(l,u,v,locx,locy,user),
-               ddH   = DdeltaDgH(gH,gb,user->Gamma,n);
-    return ddH.x * dgHdl.x + ddH.y * dgHdl.y;
+PetscReal DdeltaDl(Grad gH, Grad gb, Grad dgHdl, const AppCtx *user) {
+    const PetscReal sx  = gH.x + gb.x,
+                    sy  = gH.y + gb.y,
+                    n   = ncont(user),
+                    tmp = user->Gamma * (n-1) * PetscPowReal(sx*sx + sy*sy,(n-3.0)/2);
+    return tmp * sx * dgHdl.x + tmp * sy * dgHdl.y;
 }
 
 /*
@@ -92,17 +74,11 @@ point (x,y) on element u,v.  Since  Dcont = (1-eps) delta H^{n+2} + eps D0:
    d Dcont / dl = (1-eps) [ (d delta / dl) H^{n+2} + delta (n+2) H^{n+1} (d H / dl) ]
                 = (1-eps) H^{n+1} [ (d delta / dl) H + delta (n+2) (d H / dl) ]
 */
-PetscReal DDcontDl(PetscInt l,
-                   PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                   PetscReal delta,
-                   PetscReal **H, PetscReal **b, const AppCtx *user) {
+PetscReal DDcontDl(PetscReal delta, PetscReal ddeltadl, PetscReal H, PetscReal dHdl,
+                   const AppCtx *user) {
     const PetscReal n = ncont(user);
-    // get d/dl values:
-    const PetscReal Hatpt    = fieldatpt(u,v,locx,locy,H,user),
-                    Hpow     = PetscPowReal(Hatpt,n+1.0),
-                    dHdl     = dfieldatpt(l,u,v,locx,locy,user),
-                    ddeltadl =   DdeltaDl(l,u,v,locx,locy,H,b,user);
-    return (1.0-user->eps) * Hpow * ( ddeltadl * Hatpt + delta * (n+2.0) * dHdl );
+    const PetscReal Hpow = PetscPowReal(H,n+1.0);
+    return (1.0-user->eps) * Hpow * ( ddeltadl * H + delta * (n+2.0) * dHdl );
 }
 
 /*
@@ -111,39 +87,42 @@ Derivative of pseudo-velocity W with respect to nodal value H_l, at point
    d W.x / dl = - (d delta / dl) * gb.x
    d W.y / dl = - (d delta / dl) * gb.y
 */
-Grad DWDl(PetscInt l,
-          PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-          PetscReal **H, PetscReal **b, const AppCtx *user) {
-    const Grad gb = gradfatpt(u,v,locx,locy,b,user);
-    const PetscReal ddeltadl = DdeltaDl(l,u,v,locx,locy,H,b,user);
+Grad DWDl(PetscReal ddeltadl, Grad gb) {
     Grad dWdl;
     dWdl.x = - ddeltadl * gb.x;
     dWdl.y = - ddeltadl * gb.y;
     return dWdl;
 }
 
-
-//FIXME since  q.x = - D gH.x + W.x Hup^{n+2}  we have:
-//   d q.x / dl = - (d D / dl) gH.x - D (d gH.x / dl) + (d W.x / dl) Hup^{n+2} + W.x (n+2) Hup^{n+1} (d Hup / dl)
-// where D = Dcont and n = ncont
-/*
-PetscReal DfluxDl(PetscInt l,
-                  PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                  PetscReal Hup, PetscReal dHupdl,
-                  PetscReal **H, PetscReal **b, const AppCtx *user) {
-    const Grad gH = gradfatpt(u,v,locx,locy,H,user),
-               gb = gradfatpt(u,v,locx,locy,b,user),
-               dgHdl = dgradfatpt(l,u,v,locx,locy,user),
-               dWdl  = DWDl(l,u,v,locx,locy,H,b,user);
-    const PetscReal n     = ncont(user),
-                    Hatpt = fieldatpt(u,v,locx,locy,H,user),
-                    delta = getdelta(gH,gb,user->Gamma,n),
-                    D     = Dcont(delta,Hatpt,user);
-    const Grad W = getW(delta,gb,user);
-    // get d/dl values:
-    const PetscReal dDdl = DDcontDl(l,u,v,locx,locy,delta,H,b,user),
-    dHdl     = dfieldatpt(l,u,v,locx,locy,user),
-                    ddeltadl =   DdeltaDl(l,u,v,locx,locy,H,b,user);
-    return (1.0-user->eps) * Hpow * ( ddeltadl * Hatpt + delta * (n+2.0) * dHdl );
-}
+/* See sia.h for doc. */
+/* Caller computes (see q1op.h):
+    const Grad      gH       = gradfatpt(u,v,locx,locy,HH,user),
+                    gb       = gradfatpt(u,v,locx,locy,bb,user),
+                    dgHdl    = dgradfatpt(l,u,v,locx,locy,user);
+    const PetscReal H        = fieldatpt(u,v,locx,locy,HH,user),
+                    dHdl     = dfieldatpt(l,u,v,locx,locy,user),
+                    Hup      = fieldatpt(u,v,locxup,locyup,HH,user),
+                    dHupdl   = dfieldatpt(l,u,v,locxup,locyup,user);
 */
+PetscReal DfluxDl(Grad gH, Grad gb, Grad dgHdl,
+                  PetscReal H, PetscReal dHdl,
+                  PetscReal Hup, PetscReal dHupdl,
+                  PetscBool xdir, const AppCtx *user) {
+    const PetscReal n        = ncont(user),
+                    delta    = getdelta(gH,gb,user->Gamma,n),
+                    ddeltadl = DdeltaDl(gH,gb,dgHdl,user);
+    const Grad      W        = getW(delta,gb),
+                    dWdl     = DWDl(ddeltadl,gb);
+    const PetscReal D        = Dcont(delta,H,user),
+                    dDdl     = DDcontDl(delta,ddeltadl,H,dHdl,user),
+                    Huppow   = PetscPowReal(Hup,n+1.0),
+                    Huppow2  = Huppow * Hup,
+                    dHuppow  = (n+2.0) * Huppow * dHupdl;
+    if (xdir == PETSC_TRUE) {
+        return - dDdl * gH.x - D * dgHdl.x + dWdl.x * Huppow2 + W.x * dHuppow;
+    } else {
+        return - dDdl * gH.y - D * dgHdl.y + dWdl.y * Huppow2 + W.y * dHuppow;
+    }
+
+}
+
--- petsc/sia.h
+++ petsc/sia.h
@@ -26,7 +26,25 @@ continuation scheme.
 
 Side effect: we update maximum of diffusivity (user->maxD).
 */
-PetscReal getflux(Grad gH, Grad gb, PetscReal H, PetscReal Hup,
+PetscReal getflux(Grad gH, Grad gb,
+                  PetscReal H,
+                  PetscReal Hup,
                   PetscBool xdir, AppCtx *user);
+
+/*
+Derivative of flux (vector) with respect to nodal value H_l, at point
+(x,y) on element u,v.  Since (above)
+   q.x = - D gH.x + W.x Hup^{n+2},
+we have:
+   d q.x / dl = - (d D / dl) gH.x - D (d gH.x / dl)
+                + (d W.x / dl) Hup^{n+2} + W.x (n+2) Hup^{n+1} (d Hup / dl),
+where D = Dcont and n = ncont, and similar for d q.y / dl.
+
+Calls hidden methods to compute  D,  W,  d D / dl,  d W / dl.
+*/
+PetscReal DfluxDl(Grad gH, Grad gb, Grad dgHdl,
+                  PetscReal H, PetscReal dHdl,
+                  PetscReal Hup, PetscReal dHupdl,
+                  PetscBool xdir, const AppCtx *user);
 
 #endif // SIA_H_
