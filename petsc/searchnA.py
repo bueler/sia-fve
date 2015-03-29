94eb800c404fb9b5db1aec189a302b7585248711
Ed Bueler <elbueler@alaska.edu>
2015-03-29 11:00:48 -0600

    Draft of code to search for optimal (n,A) given e.g. Greenland.   See various FIXMEs for where this is going.

---
+++ petsc/searchnA.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+#
+# (C) 2015 Ed Bueler
+#
+# Example of usage: FIXME
+
+import argparse
+
+#parser = argparse.ArgumentParser(description='Generate .png figures from PETSc binary files written by mahaffy.c.')
+#parser.add_argument("--profile", action="store_true", help="generate profile figure")
+#args = parser.parse_args()
+
+import sys
+import numpy
+
+#from scipy.optimize import minimize
+
+_status_message = {'success': 'Optimization terminated successfully.',
+                   'maxfev': 'Maximum number of function evaluations has '
+                              'been exceeded.',
+                   'maxiter': 'Maximum number of iterations has been '
+                              'exceeded.',
+                   'pr_loss': 'Desired error not necessarily achieved due '
+                              'to precision loss.'}
+
+# following is modified _minimize_neldermead() from
+#   https://github.com/scipy/scipy/blob/v0.14.0/scipy/optimize/optimize.py
+def neldermead(func, x0, xtol=1e-4, ftol=1e-4, maxiter=None, disp=False):
+    """
+    Minimization of scalar function of one or more variables using the
+    Nelder-Mead algorithm.
+
+    Options for the Nelder-Mead algorithm are:
+        disp : bool
+            Set to True to print convergence messages.
+        xtol : float
+            Relative error in solution `xopt` acceptable for convergence.
+        ftol : float
+            Relative error in ``fun(xopt)`` acceptable for convergence.
+        maxiter : int
+            Maximum number of iterations to perform.
+    """
+
+    N = len(x0)
+    rank = len(x0.shape)
+    if not -1 < rank < 2:
+        raise ValueError("Initial guess must be a scalar or rank-1 sequence.")
+    if maxiter is None:
+        maxiter = N * 200
+
+    rho = 1
+    chi = 2
+    psi = 0.5
+    sigma = 0.5
+    one2np1 = list(range(1, N + 1))
+
+    # FIXME: following block of code generates initial simplex through magic
+    # numbers; I want the initial simplex to be an input to function
+    if rank == 0:
+        sim = numpy.zeros((N + 1,), dtype=x0.dtype)
+    else:
+        sim = numpy.zeros((N + 1, N), dtype=x0.dtype)
+    fsim = numpy.zeros((N + 1,), float)
+    sim[0] = x0
+    fsim[0] = func(x0)
+    nonzdelt = 0.05
+    zdelt = 0.00025
+    for k in range(0, N):
+        y = numpy.array(x0, copy=True)
+        if y[k] != 0:
+            y[k] = (1 + nonzdelt)*y[k]
+        else:
+            y[k] = zdelt
+
+        sim[k + 1] = y
+        f = func(y)
+        fsim[k + 1] = f
+
+    ind = numpy.argsort(fsim)
+    fsim = numpy.take(fsim, ind, 0)
+    # sort so sim[0,:] has the lowest function value
+    sim = numpy.take(sim, ind, 0)
+
+    iterations = 1
+
+    while (iterations < maxiter):
+        if (numpy.max(numpy.ravel(numpy.abs(sim[1:] - sim[0]))) <= xtol and
+                numpy.max(numpy.abs(fsim[0] - fsim[1:])) <= ftol):
+            break
+
+        xbar = numpy.add.reduce(sim[:-1], 0) / N
+        xr = (1 + rho) * xbar - rho * sim[-1]
+        fxr = func(xr)
+        doshrink = 0
+
+        if fxr < fsim[0]:
+            xe = (1 + rho * chi) * xbar - rho * chi * sim[-1]
+            fxe = func(xe)
+
+            if fxe < fxr:
+                sim[-1] = xe
+                fsim[-1] = fxe
+            else:
+                sim[-1] = xr
+                fsim[-1] = fxr
+        else:  # fsim[0] <= fxr
+            if fxr < fsim[-2]:
+                sim[-1] = xr
+                fsim[-1] = fxr
+            else:  # fxr >= fsim[-2]
+                # Perform contraction
+                if fxr < fsim[-1]:
+                    xc = (1 + psi * rho) * xbar - psi * rho * sim[-1]
+                    fxc = func(xc)
+
+                    if fxc <= fxr:
+                        sim[-1] = xc
+                        fsim[-1] = fxc
+                    else:
+                        doshrink = 1
+                else:
+                    # Perform an inside contraction
+                    xcc = (1 - psi) * xbar + psi * sim[-1]
+                    fxcc = func(xcc)
+
+                    if fxcc < fsim[-1]:
+                        sim[-1] = xcc
+                        fsim[-1] = fxcc
+                    else:
+                        doshrink = 1
+
+                if doshrink:
+                    for j in one2np1:
+                        sim[j] = sim[0] + sigma * (sim[j] - sim[0])
+                        fsim[j] = func(sim[j])
+
+        ind = numpy.argsort(fsim)
+        sim = numpy.take(sim, ind, 0)
+        fsim = numpy.take(fsim, ind, 0)
+        iterations += 1
+
+    x = sim[0]
+    fval = numpy.min(fsim)
+    warnflag = 0
+
+    if iterations >= maxiter:
+        warnflag = 2
+        msg = _status_message['maxiter']
+        if disp:
+            print('Warning: ' + msg)
+    else:
+        msg = _status_message['success']
+        if disp:
+            print(msg)
+            print("         Current function value: %f" % fval)
+            print("         Iterations: %d" % iterations)
+
+    return x
+
+
+# FIXME: replace this test function with calling mahaffy with options
+#   -mah_n n -mah_A A
+# and returning L^1 error norm as function value
+def rosen(x):
+    """The Rosenbrock function"""
+    return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+
+# FIXME: initial simplex for rosen()
+#sim0 = numpy.array([[1.3, 0.7, 0.8, 1.9, 1.2],
+#                    [1.4, 0.7, 0.8, 1.9, 1.2],
+#                    [1.3, 0.8, 0.8, 1.9, 1.2],
+#                    [1.3, 0.7, 0.9, 1.9, 1.2],
+#                    [1.3, 0.7, 0.8, 2.0, 1.2],
+#                    [1.3, 0.7, 0.8, 1.9, 1.3]])
+
+# FIXME: initial simplex for mahaffy():
+#secpera = 31556926.0
+#A0      = 1.0e-16/secpera
+#sim0 = numpy.array([[3.0,A0],
+#                    [3.5,A0],
+#                    [3.0,2.0*A0]])
+
+x0 = numpy.array([1.3, 0.7, 0.8, 1.9, 1.2])
+
+res = neldermead(rosen, x0, xtol=1e-8, disp=True)
+
+print res
+
