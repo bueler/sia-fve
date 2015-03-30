35d0963439b82c180429c8dea2d2737bed1aca63
Ed Bueler <elbueler@alaska.edu>
2015-03-30 13:37:20 -0600

    Put Q1 ops on standard unit square element.

--- petsc/q1op.c
+++ petsc/q1op.c
@@ -2,61 +2,42 @@
 
 #include "q1op.h"
 
+#define WEIGHTS \
+const PetscReal x[4] = { 1.0-xi,      xi,  xi, 1.0-xi}, \
+                y[4] = {1.0-eta, 1.0-eta, eta,    eta};
 
-void weights(PetscReal dx, PetscReal dy, PetscReal locx, PetscReal locy,
-             PetscReal (*x)[4], PetscReal (*y)[4], PetscReal (*gx)[4], PetscReal (*gy)[4]) {
-  const PetscReal xi  = locx / dx,
-                  eta = locy / dy;
-  if (x) {
-    (*x)[0]  = 1.0 - xi;   (*x)[1]  = xi;         (*x)[2]  = xi;       (*x)[3]  = 1.0 - xi;
-  }
-  if (gx) {
-    (*gx)[0] = - 1.0 / dx; (*gx)[1] = 1.0 / dx;   (*gx)[2] = 1.0 / dx; (*gx)[3] = - 1.0 / dx;
-  }
-  if (y) {
-    (*y)[0]  = 1.0 - eta;  (*y)[1]  = 1.0 - eta;  (*y)[2]  = eta;      (*y)[3]  = eta;
-  }
-  if (gy) {
-    (*gy)[0] = - 1.0 / dy; (*gy)[1] = - 1.0 / dy; (*gy)[2] = 1.0 / dy; (*gy)[3] = 1.0 / dy;
-  }
-}
+#define GWEIGHTS \
+const PetscReal gx[4] = {-1.0,  1.0, 1.0, -1.0}, \
+                gy[4] = {-1.0, -1.0, 1.0,  1.0};
 
-PetscReal fieldatpt(PetscInt u, PetscInt v,         // (j,k) is the element (by lower-left corner)
-                    PetscReal locx, PetscReal locy, // = (x,y) coords in element
-                    PetscReal **f,                  // f[k][j] are node values
-                    const AppCtx *user) {
-  PetscReal x[4], y[4];
-  weights(user->dx,user->dx,locx,locy,&x,&y,NULL,NULL);
+PetscReal fieldatpt(PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal **f) {
+  WEIGHTS
   return x[0] * y[0] * f[v][u] + x[1] * y[1] * f[v][u+1] + x[2] * y[2] * f[v+1][u+1] + x[3] * y[3] * f[v+1][u];
 }
 
-Grad gradfatpt(PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-               PetscReal **f, const AppCtx *user) {
-  PetscReal x[4], y[4], gx[4], gy[4];
+Grad gradfatpt(PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal dx, PetscReal dy, PetscReal **f) {
   Grad gradf;
-  weights(user->dx,user->dx,locx,locy,&x,&y,&gx,&gy);
+  WEIGHTS
+  GWEIGHTS
   gradf.x =   gx[0] * y[0] * f[v][u]     + gx[1] * y[1] * f[v][u+1]
             + gx[2] * y[2] * f[v+1][u+1] + gx[3] * y[3] * f[v+1][u];
   gradf.y =    x[0] *gy[0] * f[v][u]     +  x[1] *gy[1] * f[v][u+1]
             +  x[2] *gy[2] * f[v+1][u+1] +  x[3] *gy[3] * f[v+1][u];
+  gradf.x /= dx;
+  gradf.y /= dy;
   return gradf;
 }
 
-PetscReal dfieldatpt(PetscInt l,
-                     PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                     const AppCtx *user) {
-  PetscReal x[4], y[4];
-  weights(user->dx,user->dx,locx,locy,&x,&y,NULL,NULL);
+PetscReal dfieldatpt(PetscInt l, PetscInt u, PetscInt v, PetscReal xi, PetscReal eta) {
+  WEIGHTS
   return x[l] * y[l];
 }
 
-Grad dgradfatpt(PetscInt l,
-                PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                const AppCtx *user) {
-  PetscReal x[4], y[4], gx[4], gy[4];
+Grad dgradfatpt(PetscInt l, PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal dx, PetscReal dy) {
   Grad dgradfdl;
-  weights(user->dx,user->dx,locx,locy,&x,&y,&gx,&gy);
-  dgradfdl.x = gx[l] *  y[l];
-  dgradfdl.y =  x[l] * gy[l];
+  WEIGHTS
+  GWEIGHTS
+  dgradfdl.x = gx[l] *  y[l] / dx;
+  dgradfdl.y =  x[l] * gy[l] / dy;
   return dgradfdl;
 }
--- petsc/q1op.h
+++ petsc/q1op.h
@@ -6,12 +6,14 @@
 #define Q1OP_H_
 
 #include <petscvec.h>
-#include "mahaffyctx.h"  // for Grad and AppCtx
+#include "mahaffyctx.h"  // for Grad only
 
 /*
 In the following functions
    * f(x,y) is represented by PetscReal** array for DMDA Vec, i.e. by {f[k][j]}
-   * use local coordinates:  locx = x-x_u, locy = y-y_v
+   * we are evaluating on element \square_{u,v}
+   * use local coordinates:  xi = (x-x_u)/dx, eta = (y-y_v)/dy
+       so (xi,eta) is in [0,1]x[0,1]
    * l indexes corner in counter-clockwise order:
        l=0 is (x_u,    y_v)
        l=1 is (x_{u+1},y_v)
@@ -20,23 +22,17 @@ In the following functions
 */
 
 // evaluate scalar field  f(x,y)  at point (x,y) on element u,v
-PetscReal fieldatpt(PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                    PetscReal **f, const AppCtx *user);
+PetscReal fieldatpt(PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal **f);
 
 // evaluate gradient  (\nabla f)(x,y)  at point (x,y) on element u,v
-Grad gradfatpt(PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-               PetscReal **f, const AppCtx *user);
+Grad      gradfatpt(PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal dx, PetscReal dy, PetscReal **f);
 
 // evaluate derivative of scalar field  f(x,y)  with respect to nodal value f_l
 // at point (x,y) on element u,v
-PetscReal dfieldatpt(PetscInt l,
-                     PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                     const AppCtx *user);
+PetscReal dfieldatpt(PetscInt l, PetscInt u, PetscInt v, PetscReal xi, PetscReal eta);
 
 // evaluate derivative of gradient   (\nabla f)(x,y)  with respect to nodal value f_l
 // at point (x,y) on element u,v
-Grad dgradfatpt(PetscInt l,
-                PetscInt u, PetscInt v, PetscReal locx, PetscReal locy,
-                const AppCtx *user);
+Grad      dgradfatpt(PetscInt l, PetscInt u, PetscInt v, PetscReal xi, PetscReal eta, PetscReal dx, PetscReal dy);
 
 #endif // Q1OP_H_
