b60851b2e7ae7895e792299cafeae06fca056993
Ed Bueler <elbueler@alaska.edu>
2015-04-02 22:33:59 -0600

    Switch to easier-to-play-with resolution.  TODO: learn how to use CDO remapbil or similar.

--- petsc/grn/getmcb.sh
+++ petsc/grn/getmcb.sh
@@ -2,5 +2,6 @@
 
 wget -nc ftp://sidads.colorado.edu/DATASETS/IDBMG4_BedMachineGr/MCdataset-2014-11-19.nc
 
-ncks -v x,y,thickness,bed MCdataset-2014-11-19.nc -d x,,,6 -d y,,,6 mcb900m.nc
+#ncks -v x,y,thickness,bed MCdataset-2014-11-19.nc -d x,,,6 -d y,,,6 mcb900m.nc
+ncks -v x,y,thickness,bed MCdataset-2014-11-19.nc -d x,,,30 -d y,,,30 mcb4500m.nc
 
