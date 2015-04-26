fe5ca879b70372502f7f6d171f14d5466d3e04c5
Ed Bueler <elbueler@alaska.edu>
2015-04-25 19:42:16 -0600

    Improve i/o of shadesurf.py script, including control over dpi.

--- petsc/shadesurf.py
+++ petsc/shadesurf.py
@@ -12,8 +12,12 @@ from matplotlib.colors import LightSource
 import argparse
 
 parser = argparse.ArgumentParser(description='Generate shaded figure of surface elevation from PETSc binary file written by mahaffy.c.')
-parser.add_argument('-i', default='unnamed.dat', metavar='NAME', type=str,
-                    help='name of special format PETSc binary file from which to read; default=%(default)s)')
+parser.add_argument('-o', metavar='FILENAME',
+                    help='image filename (e.g. .png or .pdf)')
+parser.add_argument('inname', metavar='FILE.dat', type=str,
+                    help='name of .dat input file')
+parser.add_argument('-dpi', default=200, type=int,
+                    help='save figure at this resolution')
 args = parser.parse_args()
 
 try:
@@ -22,15 +26,15 @@ except:
     print "importing petsc2numpy failed"
     sys.exit(2)
 
-print "reading from file %s ..." % args.i
-d = readvecs(args.i,num=2,metaname='dimension variable',names=['x','y'])
+print "reading from file %s ..." % args.inname
+d = readvecs(args.inname,num=2,metaname='dimension variable',names=['x','y'])
 x,y = d[0],d[1]
-v = readvecs(args.i,num=7,skip=2,shape=(len(y),len(x)),names=['b','m','Hexact','H','residual'])
+v = readvecs(args.inname,num=7,skip=2,shape=(len(y),len(x)),names=['b','m','Hexact','H','residual'])
 #b,_,_,H,_ = v[0],v[1],v[2],v[3],v[4]
 b = v[0]
 H = v[3]
 
-print 'read b,H from file %s; shapes (%d,%d)' % (args.i,len(y),len(x))
+print 'read b,H from file %s; shapes (%d,%d)' % (args.inname,len(y),len(x))
 
 def gets(H,b):
     Hdraft = -(910.0/1028.0) * H
@@ -62,5 +66,10 @@ print 'figure generated ...'
 #plt.title('imshow with shading')
 plt.xticks([])
 plt.yticks([])
-plt.show()
+
+if args.o == None:
+    plt.show()
+else:
+    plt.savefig(args.o,dpi=args.dpi,bbox_inches='tight')
+
 
